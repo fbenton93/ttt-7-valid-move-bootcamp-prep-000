653c483a803e45171c7783359f68fc6d63e2d1d2
Forrest Benton <elliottbenton@gmail.com>
2018-06-19 16:05:35 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,10 @@
-
+def position_taken?(board,index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
 
 def valid_move?(board,index)
   if !(position_taken?(board,index)) && index.between?(0,8)
